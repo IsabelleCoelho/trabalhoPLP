0e3b949eadd2c9fdfe19b5774aaece3d8f7f2fe7
RenatoPereira2600 <renatopereira2600@gmail.com>
2019-05-01 17:08:20 -0300

    Fiz a classe Visitante malakois

--- Visitante.java
+++ Visitante.java
@@ -1,5 +1,12 @@
+import java.util.*;
 public class Visitante extends Pessoa {
-
-	private List<Visita> listaVisitas;
+	private ArrayList<Visita> listaVisitas;
+	public Visitante(String nome){
+		super(nome);
+		listaVisitas = new ArrayList<Visita>();
+	}
+	public bolean adicionarVisita(Visita umaVisita){
+		return listaVisitas.add(umaVisita);
+	}
 
 }
