9ee137ed4b5e4df314a7ed5c0fddccac029011bb
IsabelleCoelho <isabellerc37@gmail.com>
2019-05-01 16:00:18 -0300

    Alterada a classe Setor.

--- Setor.java
+++ Setor.java
@@ -1,7 +1,32 @@
+import java.util.*;
+
 public class Setor {
 
 	private String tipoDeExibicao;
-
 	private String nomeSetor;
 
+	private ArrayList<Colecao> colecoes;
+
+	/** Definindo o construtor */
+	public Setor(String tipoDeExibicao, String nomeSetor){
+		this.tipoDeExibicao = tipoDeExibicao;
+		this.nomeSetor = nomeSetor;
+		this.colecoes = new ArrayList<Colecao>();
+	}
+
+	/** Adicionando uma nova peça a um setor */
+	public void adicionarPeca(Colecao umaColecao) {
+		colecoes.add(umaColecao);
+	}
+
+	@Override
+	public String toString(){
+		String out = "Setor " + nomeSetor + " exibe ";
+		if(tipoDeExibicao.equals("temporario")){
+			out = out + " temporariamente ";
+		}
+		for (Colecao umaColecao : colecoes) {
+			out = out + umaColecao.nomeColecao;
+		}
+	}
 }
