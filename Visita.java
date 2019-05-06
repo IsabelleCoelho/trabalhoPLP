1b05cedfc36a4d50f7a384c7c8cdba3bdd1e85df
Aluno <vitorwixmix@gmail.com>
2019-05-06 15:16:10 -0300

    Adicionando o atributo visitante

--- Visita.java
+++ Visita.java
@@ -1,15 +1,18 @@
 import java.util.*;
 
 public class Visita {
-	private ArrayList<Setor> listaSetores;
-	private Data dataVisitacao;
+    private ArrayList<Setor> listaSetores;
+    private Data dataVisitacao;
+    private Visitante pessoaVisitante;
 
-	public Visita(Data dataVisitacao){
-		this.dataVisitacao = dataVisitacao;
-		this.listaSetores = new ArrayList<Setor>();
-	}
+    public Visita(Data dataVisitacao, listaSetoresVisitados
+                  Visitante pessoaVisitante){
+        this.dataVisitacao = dataVisitacao;
+        this.listaSetores = new ArrayList<Setor>();
+        this.pessoaVisitante = pessoaVisitante;
+    }
 
-	public boolean adicionarSetorVisitado(Setor umSetor){
-		return listaSetores.add(umSetor);
-	}
+    public boolean adicionarSetorVisitado(Setor umSetor){
+        return listaSetores.add(umSetor);
+    }
 }
